9cac5d095131dc64470faf40ee362fa6ddc0af11
Monu_2114 <monu.nanabala@gmail.com>
2024-08-19 23:50:00 +0530

    connection

--- db/pool.js
+++ db/pool.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.DB_CONNECTION || process.env.MONGO_URL,
-      {}
-    );
+    const db =
+      process.env.NODE_env == "production"
+        ? process.env.DB_CONNECTION
+        : process.env.LOCAL_CONNECTION;
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
   } catch (err) {
     console.error("Failed to connect to MongoDB", err.message);
