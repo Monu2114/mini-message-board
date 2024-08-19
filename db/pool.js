f995d7a6d9a46635b19527f335b9361815cbbd10
Monu_2114 <monu.nanabala@gmail.com>
2024-08-20 00:03:46 +0530

    gitignore updated

--- db/pool.js
+++ db/pool.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+
 const connectDB = async () => {
   try {
-    const db =
-      process.env.NODE_ENV == "production"
+    const dbUri =
+      process.env.NODE_ENV === "production"
         ? process.env.DB_CONNECTION
         : process.env.LOCAL_CONNECTION;
-    await mongoose.connect(db, {
+
+    await mongoose.connect(dbUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log("MongoDB connected");
   } catch (err) {
     console.error("Failed to connect to MongoDB", err.message);
